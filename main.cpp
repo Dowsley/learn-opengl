c3d9559a5ec14024239c890ef7ce26cb9dc10b2b
Dowsley <joao.dowsley850@gmail.com>
2024-05-21 12:38:33 -0300

    Chapter 4: First working window

--- main.cpp
+++ main.cpp
@@ -1,13 +1,51 @@
 #include <iostream>
+#include <memory>
+
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+const unsigned int WINDOW_WIDTH = 800;
+const unsigned int WINDOW_HEIGHT = 600;
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+
+int main()
+{
+    std::cout << "GLFW init..." << std::endl;
+
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+    GLFWwindow *window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "LearnOpenGL - fellsand", nullptr, nullptr);
+    if (window == nullptr) {
+    	std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    std::cout << "GLAD init..." << std::endl;
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+	    std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    std::cout << "Starting window and main loop..." << std::endl;
+    glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+    while (!glfwWindowShouldClose(window)) {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
+}
+
+/* Will serve as a callback for GLFW whenever the window is resized */
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
+{
+    glViewport(0, 0, width, height);
 }
