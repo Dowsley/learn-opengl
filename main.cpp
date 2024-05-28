d77dcef78d848254bbddace918f5572f09ebbead
Dowsley <joao.dowsley850@gmail.com>
2024-05-27 22:19:21 -0300

    Chapter 7: Funny face

--- main.cpp
+++ main.cpp
@@ -2,10 +2,8 @@
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include <fstream>
-#include <iterator>
-#include <cmath>
 #include "shader.h"
+#include "lib/stb_image.h"
 
 const unsigned int WINDOW_WIDTH = 800;
 const unsigned int WINDOW_HEIGHT = 600;
@@ -13,7 +11,6 @@ const unsigned int WINDOW_HEIGHT = 600;
 void framebufferSizeCallback(GLFWwindow *window, int width, int height);
 void processInput(GLFWwindow *window);
 bool isKeyPressed(GLFWwindow *window, int key);
-unsigned int createPrimitiveTriangleVAO(float vertices[], int size);
 
 int main()
 {
@@ -46,14 +43,92 @@ int main()
 
 
     /* 3. OpenGL: Initializing shaders and objects */
+    // Square (position, color, texture coord))
     float vertices[] = {
-        0.0, 0.5, 0.0,      1.0, 0.0, 0.0,
-        -0.5, -0.5, 0.0,        0.0, 1.0, 0.0,
-        0.5,-0.5, 0.0,      0.0, 0.0, 1.0
+        0.5, 0.5, 0.0,      1.0, 0.0, 0.0,      1.0, 1.0, // top right
+        0.5,-0.5, 0.0,      0.0, 1.0, 1.0,      1.0, 0.0, // bottom right
+        -0.5, -0.5, 0.0,        0.0, 0.0, 1.0,      0.0, 0.0, // bottom left
+        -0.5,0.5, 0.0,      1.0, 1.0, 0.0,       0.0, 1.0, //  top left
     };
-    unsigned int VAO = createPrimitiveTriangleVAO(vertices, sizeof(vertices));
+    unsigned int indices[] = {
+            0, 1, 3, // first triangle
+            1, 2, 3  // second triangle
+    };
+
+    /*
+     * 3.1 Texture Setup
+     * */
+    unsigned int texture1;
+    glGenTextures(1, &texture1);
+
+    glBindTexture(GL_TEXTURE_2D, texture1);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    int width, height, nChannels;
+    stbi_set_flip_vertically_on_load(true);
+    unsigned char *data = stbi_load("assets/textures/container.jpg", &width, &height, &nChannels, 0);
+    if (data) {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    } else {
+        std::cout << "Failed to load texture." << std::endl;
+    }
+    stbi_image_free(data);
+
+    unsigned int texture2;
+    glGenTextures(1, &texture2);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    data = stbi_load("assets/textures/awesomeface.png", &width, &height, &nChannels, 0);
+    if (data) {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    } else {
+        std::cout << "Failed to load texture." << std::endl;
+    }
+    stbi_image_free(data);
+
+
+    /*
+     * 3.2 VAO and VBO setup
+     * */
+    unsigned int VAO, VBO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    // Set the vertex attribute pointers
+    int stride = 8 * sizeof(float);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, stride, (void*)0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, stride, (void*) (3*sizeof(float)));
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, stride, (void*) (6*sizeof(float)));
+
+    glEnableVertexAttribArray(0);
+    glEnableVertexAttribArray(1);
+    glEnableVertexAttribArray(2);
+
 
     Shader ourShader("shaders/vertexShader.glsl", "shaders/fragmentShader.glsl");
+
+    ourShader.use();
+    // either set it manually like so:
+    glUniform1i(glGetUniformLocation(ourShader.ID, "texture1"), 0);
+    // or set it via the texture class
+    ourShader.setInt("texture2", 1);
 
     glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
     while (!glfwWindowShouldClose(window)) {
@@ -64,13 +139,24 @@ int main()
 
         ourShader.use();
 
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture1);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture2);
+
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         glBindVertexArray(0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+
+    // optional: de-allocate all resources once they've outlived their purpose:
+    // ------------------------------------------------------------------------
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
 
     glfwTerminate();
     return 0;
@@ -90,29 +176,6 @@ void processInput(GLFWwindow *window) {
 
 bool isKeyPressed(GLFWwindow *window, int key) {
     return glfwGetKey(window, key) == GLFW_PRESS;
-}
-
-unsigned int createPrimitiveTriangleVAO(float vertices[], int size)
-{
-    /* We create and bind a VAO first, so it registers the VBO and Vertex Attribute calls. */
-    unsigned int VAO;
-    glGenVertexArrays(1, &VAO);
-    glBindVertexArray(VAO);
-
-    /* Copy the vertex data to the actual VBO and bind it */
-    unsigned int VBO;
-    glGenBuffers(1, &VBO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    // Static draw because the triangle won't change for now.
-    glBufferData(GL_ARRAY_BUFFER, size, vertices, GL_STATIC_DRAW);
-
-    /* Set the vertex attribute pointers */
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*) (3*sizeof(float)));
-    glEnableVertexAttribArray(0);
-    glEnableVertexAttribArray(1);
-
-    return VAO;
 }
 
 
